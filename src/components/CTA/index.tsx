444d061f56d90e0a6ae3a56f770a457a3b4128a6
quilantera <gugaquilante@gmail.com>
2023-09-25 11:44:20 -0300

    adicionando o curriculo

--- src/components/CTA/index.tsx
+++ src/components/CTA/index.tsx
@@ -3,7 +3,7 @@ import './styles.scss'
 export function CTA(){
     return(
     <div className="cta">
-        <a href="cv2022.pdf" download="" className="btn">Download CV</a>
+        <a href="cv2023.pdf" download="" className="btn">Download CV</a>
         <a href="#contact" className="btn btn-primary">Let's talk</a>
     </div>
     )
